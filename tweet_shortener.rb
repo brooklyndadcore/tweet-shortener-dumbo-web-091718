52fdd361ebcd781607eeeaae6b7372fb3a248874
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-24 13:59:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,42 @@
-# Write your code here.
+def word_substituter(string)
+  words = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2', 
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' =>'u',
+    "at" => "@", 
+    "and" => "&"
+  }
+  tweet_arr = []
+  arr = string.split(" ")
+  arr.collect! do |word|
+    if words[word]   
+      tweet_arr << words[word]
+    else
+      tweet_arr << word
+    end
+  end
+  return tweet_arr.join(" ")
+end
+
+
+def bulk_tweet_shortener(array)
+  arr = []
+  array.each {|twt| arr<< word_substituter(twt)}
+  arr.each {|ele| puts ele}
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word =  word_substituter(tweet) : word =  tweet
+  return word
+end
+
+def shortened_tweet_truncator(tweet)
+  word = word_substituter(tweet)
+  word.length > 140 ? word = word.slice(0...140) : word = word
+  return word
+end
